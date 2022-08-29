99db89f26ce97ff54064733d9a85ec4863d2d4c4
Vinicius Assis <vinicius.coassis@gmail.com>
2022-08-29 14:32:07 -0300

    final version

--- src/app.js
+++ src/app.js
@@ -5,19 +5,8 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-let users = [
-  {
-    username: "bobesponja",
-    avatar:
-      "https://www.cnnbrasil.com.br/wp-content/uploads/sites/12/2021/12/GettyImages-1205198865.jpg",
-  },
-];
-let tweets = [
-  {
-    username: "bobesponja",
-    tweet: "eu amo o hub",
-  },
-];
+let users = [];
+let tweets = [];
 
 app.get("/sign-up", (req, res) => {
   res.send(users);
@@ -29,8 +18,25 @@ app.post("/sign-up", (req, res) => {
   res.send("OK");
 });
 
+app.post("/tweets", (req, res) => {
+  if (!req.body.username) {
+    res.status(404).send("Usuário não esta sendo enviado");
+  }
+  if (!req.body.tweet) {
+    res.status(404).send("Tweet não esta sendo enviado");
+  }
+
+  tweets.push(req.body);
+  res.send("OK");
+});
+
 app.get("/tweets", (req, res) => {
-  users.filter((value) => value.username == tweets.username);
+  for (let i = 0; i < tweets.length; i++) {
+    let nick = tweets[i].username;
+    let avatar = users.find((value) => value.username == nick).avatar;
+    tweets[i].avatar = avatar;
+  }
+  tweets.splice(10);
   res.send(tweets);
 });
 
